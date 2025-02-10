98b3969a7c6e0e050e814be78bd6044c8fa27346
munendra-raghuvnashi <munendraraghuvanshi@gmail.com>
2025-02-10 23:28:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Hero from "./components/Hero";
+import About from "./components/About";
+import NavBar from './components/NavBar';
+import Features from './components/Features';
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+
+const App = () => {
+  return (
+    <div className="relative min-h-screen w-screen overflow-x-hidden">
+     <Hero />
+     <About />
+     <NavBar />
+     <Features />
+     <Story />
+      <Contact />
+      <Footer />
+
+     
+    </div>
+  );
+}
+
+export default App
